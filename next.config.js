7668d7c36beda242b5cb8f96bcf3e6acb3289daf
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-05-10 18:01:02 +0530

    Created plugin files & added config for twailwind, eslint, prettier etc.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const libs = ['@solana/wallet-adapter-base'];
+const withTM = require('next-transpile-modules')(libs);
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const nextConfig = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    const cfg = {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        fallback: {
+          fs: false,
+          os: false,
+          path: false,
+        },
+      },
+    };
+
+    if (!isServer) {
+      cfg.optimization.splitChunks = {
+        chunks: 'all',
+        minSize: 20480,
+      };
+    }
+
+    return cfg;
+  },
+});
+
+module.exports = nextConfig;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Init } from "./types";
+
+const RenderTwamm = (props: Init) => {
+  return;
+};
+
+export { RenderTwamm };
